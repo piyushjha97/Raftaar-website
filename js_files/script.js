a79e6024a41571e3d79efbaf70c28d78a2746707
piyushjha97 <jhapiyush2013@gmail.com>
2019-04-10 19:02:57 +0530

    basic-front page desig

---
+++ js_files/script.js
@@ -0,0 +1,99 @@
+
+//when the page is scrolled this fuction is called
+ window.onscroll = function() {myfunction()};
+
+ //getting navbar
+var navbar = document.getElementById("navbar");
+
+//getting the offset position of navbar
+var sticky = navbar.offsetTop;
+
+
+//defining the function
+function myfunction ()
+{
+    if(window.pageYOffset >=sticky)
+    {
+        navbar.classList.add("sticky")
+    }
+    else
+    {
+        navbar.classList.remove("sticky");
+
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  
+
+
+
+
+
+  function getTimeRemaining(endtime) 
+  {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) 
+  {
+    var clock = document.getElementById('timer');
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  initializeClock('clockdiv', deadline);
